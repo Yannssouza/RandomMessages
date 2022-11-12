6b02f7afc2b680734a069127d5d65ed1d212ef50
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2022-11-12 09:14:25 -0300

    RandomMessage 1.0

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let randomNames = ["Carl", "Max", "Steve"];
 let randomPlaces = ["Street", "Field", "Stadium"];
-let randomAnimal = ["Dog", "Bird", "Leo"]
+let randomAnimal = ["Dog", "Bird", "Leo"];
 
 let n = Math.floor(Math.random() * 3);
 let g = Math.floor(Math.random() * 3);
