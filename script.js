8927e97d2942862209ee9b959908a12f27746a85
Yannssouza <115546740+Yannssouza@users.noreply.github.com>
2022-11-12 09:32:54 -0300

    "Function add"

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const random = {
-    names:  ["Carl", "Max", "Steve"],
+    names: ["Carl", "Max", "Steve"],
     places: ["Street", "Field", "Stadium"],
     animal: ["Dog", "Bird", "Leo"]
 }
 
-const n = Math.floor(Math.random() * 3);
-let g = Math.floor(Math.random() * 3);
-let i = Math.floor(Math.random() * 3);
+function displayMessage() {
+    const n = Math.floor(Math.random() * 3);
+    const g = Math.floor(Math.random() * 3);
+    const i = Math.floor(Math.random() * 3);
+    console.log(`${random.names[n]} was walking in the ${random.places[g]}, than he saw a ${random.animal[i]}.`);
+}
 
-console.log(`${random.names[n]} was walking in the ${random.places[g]}, than he saw a ${random.animal[i]}.`);
+displayMessage();
